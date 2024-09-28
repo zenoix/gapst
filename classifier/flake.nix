6407fac349ca918657a5c59f0735fe13ebb6ac21
zenoix <41812358+zenoix@users.noreply.github.com>
2024-09-28 22:30:48 +1200

    build(classifier): python gRPC server environment

---
+++ classifier/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      # importing package example
+      # packages."x86_64-linux".default = 
+      #   pkgs.callPackage (import ./default.nix) {};
+
+      devShells."x86_64-linux".default = pkgs.mkShell {
+
+        packages = [
+          (pkgs.python3.withPackages (
+            python-pkgs: with python-pkgs; [
+              grpcio
+              grpcio-tools
+              numpy
+              pandas
+              pandas-stubs
+              scikit-learn
+            ]
+          ))
+        ];
+
+      };
+    };
+}
