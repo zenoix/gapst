1ee4f98e194eca6631eed6eb122529b1753238e0
zenoix <41812358+zenoix@users.noreply.github.com>
2024-09-28 23:40:13 +1200

    feat(api): add basic request body handling for /predict endpoint

--- api/main.go
+++ api/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type predictionRequest struct {
+	SepalLengthCm float32
+	SepalWidthCm  float32
+	PetalLengthCm float32
+	PetalWidthCm  float32
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -14,6 +22,29 @@ func main() {
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("GET / called")
 		fmt.Fprint(w, "Server is running")
+	})
+
+	mux.HandleFunc("POST /predict", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("POST /predict called")
+
+		d := json.NewDecoder(r.Body)
+		d.DisallowUnknownFields()
+
+		var p predictionRequest
+
+		if err := d.Decode(&p); err != nil {
+			log.Printf("Error occurred while decoding JSON object - %s", err.Error())
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if d.More() {
+			log.Println("Extraneous data found after JSON object")
+			http.Error(w, "extraneous data after JSON object", http.StatusBadRequest)
+			return
+		}
+
+		log.Println(p)
 	})
 
 	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
